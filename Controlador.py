4f971deb0e34bc2d1055d990f40026571739fc9c
Emanuel Cruz Tabares <169178781+ecruzt@users.noreply.github.com>
2024-11-29 16:37:17 -0500

    controlador inicio

--- Controlador.py
+++ Controlador.py
@@ -1,3 +1,12 @@
 import sys
 from PyQt5.Qtwidgets import QApplication
+from modelo import *
+from vista import *
 
+class Controlador:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.base_datos = DataBase('')
+        self.vista = VentanaPrincipal(self.base_datos)
+        self.conectar_eventos()
+        self.vista.cargar_pacientes()
