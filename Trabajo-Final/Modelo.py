9cf607e6823a9f4e8496fdbf9832f637f4506bf4
DianaUdeA <142943455+DianaUdeA@users.noreply.github.com>
2024-12-03 23:50:24 -0500

    Add files via upload

---
+++ Trabajo-Final/Modelo.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class DataBase:
+    def __init__(self,nombre_archivo):
+        self.nombre_archivo=nombre_archivo
+        self.conexion=sqlite3.connect(nombre_archivo)
+        self.cursor=self.conexion.cursor()
+        self.crear_tabla_login()
+        self.crear_tabla_pacientes()
+
+    def crear_tabla_login(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Usuarios (
+                                Usuario TEXT,
+                                Contraseña INTEGER,
+                            )''')
+        self.conexion.commit()
+    def crear_tabla_pacientes(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Pacientes (
+                                Cedula INTEGER,
+                                Nombre TEXT,
+                                
+                            )''')
+        self.conexion.commit()    
+
+
+
+    
+
+
+
+
+        
